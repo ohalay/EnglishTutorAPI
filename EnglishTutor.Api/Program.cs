d4427e80d0afc6821f4a6d7e52368c81bec9b651
oleh.halay <oleh.halay@eleks.com>
2017-03-27 11:18:14 +0300

    -add url to Kestrel server

--- EnglishTutor.Api/Program.cs
+++ EnglishTutor.Api/Program.cs
@@ -9,10 +9,9 @@ namespace EnglishTutor.Api
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:80")
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
